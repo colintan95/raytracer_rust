00b7f10b4b10a5e2b19173fd440dc5bcb7fa643c
Colin Tan <colintan95@gmail.com>
2020-09-29 18:28:41 -0700

    Implemented translations and composition of transforms

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,54 @@ struct Transform {
 }
 
 impl Transform {
+    fn identity() -> Transform {
+        Transform {
+            a00: 1.0,
+            a01: 0.0,
+            a02: 0.0,
+            a03: 0.0,
+
+            a10: 0.0,
+            a11: 1.0,
+            a12: 0.0,
+            a13: 0.0,
+
+            a20: 0.0,
+            a21: 0.0,
+            a22: 1.0,
+            a23: 0.0,
+
+            a30: 0.0,
+            a31: 0.0,
+            a32: 0.0,
+            a33: 1.0,
+        }
+    }
+
+    fn translate(delta: Vec3) -> Transform {
+        Transform {
+            a00: 1.0,
+            a01: 0.0,
+            a02: 0.0,
+            a03: delta.x,
+
+            a10: 0.0,
+            a11: 1.0,
+            a12: 0.0,
+            a13: delta.y,
+
+            a20: 0.0,
+            a21: 0.0,
+            a22: 1.0,
+            a23: delta.z,
+
+            a30: 0.0,
+            a31: 0.0,
+            a32: 0.0,
+            a33: 1.0,
+        }
+    }
+
     // |theta| is in degrees.
     fn rotate(theta: f32, axis: Vec3) -> Transform {
         let axis = Vec3::normalize(axis);
@@ -44,7 +92,48 @@ impl Transform {
         }
     }
 
-    fn apply(&self, v: &Vec3) -> Vec3 {
+    // TODO: Replace this with multiply operator or sth.
+    fn compose(&self, other: &Transform) -> Transform {
+        Transform {
+            a00 : self.a00 * other.a00 + self.a01 * other.a10 + self.a02 * other.a20 +
+                  self.a03 * other.a30,
+            a01 : self.a00 * other.a01 + self.a01 * other.a11 + self.a02 * other.a21 +
+                  self.a03 * other.a31,
+            a02 : self.a00 * other.a02 + self.a01 * other.a12 + self.a02 * other.a22 +
+                  self.a03 * other.a32,
+            a03 : self.a00 * other.a03 + self.a01 * other.a13 + self.a02 * other.a23 +
+                  self.a03 * other.a33, 
+
+            a10 : self.a10 * other.a00 + self.a11 * other.a10 + self.a12 * other.a20 +
+                  self.a13 * other.a30,
+            a11 : self.a10 * other.a01 + self.a11 * other.a11 + self.a12 * other.a21 +
+                  self.a13 * other.a31,
+            a12 : self.a10 * other.a02 + self.a11 * other.a12 + self.a12 * other.a22 +
+                  self.a13 * other.a32,
+            a13 : self.a10 * other.a03 + self.a11 * other.a13 + self.a12 * other.a23 +
+                  self.a13 * other.a33, 
+
+            a20 : self.a20 * other.a00 + self.a21 * other.a10 + self.a22 * other.a20 +
+                  self.a23 * other.a30,
+            a21 : self.a20 * other.a01 + self.a21 * other.a11 + self.a22 * other.a21 +
+                  self.a23 * other.a31,
+            a22 : self.a20 * other.a02 + self.a21 * other.a12 + self.a22 * other.a22 +
+                  self.a23 * other.a32,
+            a23 : self.a20 * other.a03 + self.a21 * other.a13 + self.a22 * other.a23 +
+                  self.a23 * other.a33, 
+
+            a30 : self.a30 * other.a00 + self.a31 * other.a10 + self.a32 * other.a20 +
+                  self.a33 * other.a30,
+            a31 : self.a30 * other.a01 + self.a31 * other.a11 + self.a32 * other.a21 +
+                  self.a33 * other.a31,
+            a32 : self.a30 * other.a02 + self.a31 * other.a12 + self.a32 * other.a22 +
+                  self.a33 * other.a32,
+            a33 : self.a30 * other.a03 + self.a31 * other.a13 + self.a32 * other.a23 +
+                  self.a33 * other.a33, 
+        }
+    }
+
+    fn apply_vec(&self, v: &Vec3) -> Vec3 {
         Vec3 {
             x: self.a00 * v.x + self.a01 * v.y + self.a02 * v.z,
             y: self.a10 * v.x + self.a11 * v.y + self.a12 * v.z,
@@ -54,9 +143,9 @@ impl Transform {
 
     fn apply_pt(&self, p: &Point3) -> Point3 {
         Point3 {
-            x: self.a00 * p.x + self.a01 * p.y + self.a02 * p.z,
-            y: self.a10 * p.x + self.a11 * p.y + self.a12 * p.z,
-            z: self.a20 * p.x + self.a21 * p.y + self.a22 * p.z,
+            x: self.a00 * p.x + self.a01 * p.y + self.a02 * p.z + self.a03,
+            y: self.a10 * p.x + self.a11 * p.y + self.a12 * p.z + self.a13,
+            z: self.a20 * p.x + self.a21 * p.y + self.a22 * p.z + self.a23,
         }
     }
 }
@@ -92,7 +181,9 @@ fn main() {
     let mut buffer = vec![100u8; num_pixels];
     let mut rays = Vec::<Ray>::with_capacity(num_pixels);
 
-    let rotate = Transform::rotate(15.0, Vec3::new(1.0, 0.0, 0.0));
+    let rotate = Transform::rotate(45.0, Vec3::new(1.0, 0.0, 0.0));
+    let translate = Transform::translate(Vec3::new(0.0, 10.0, 0.0));
+    let transform = translate.compose(&rotate);
 
     for i in 0..img_width {
         for j in 0..img_height {
@@ -102,8 +193,8 @@ fn main() {
             let y = -((i as f32) / (img_height as f32) * 1.0 - 0.5);
 
             let ray = Ray {
-                p: rotate.apply_pt(&Point3::new(x, y, 1.0)),
-                d: rotate.apply(&Vec3::new(x, y, 1.0)),
+                p: transform.apply_pt(&Point3::new(x, y, 1.0)),
+                d: transform.apply_vec(&Vec3::new(x, y, 1.0)),
             };
 
             rays.push(ray);
@@ -111,11 +202,11 @@ fn main() {
     } 
 
     let sphere = Sphere {
-        c: Point3::new(0.0, 0.0, 20.0),
+        c: Point3::new(0.0, 0.0, 10.0),
         r: 2.5,
     };
 
-    let light_pos = Point3::new(0.0, 5.0, 5.0);
+    let light_pos = Point3::new(0.0, 5.0, 10.0);
 
     for i in 0..img_width {
         for j in 0..img_height {
